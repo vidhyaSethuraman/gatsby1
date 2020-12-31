e18bc8debc7a517b22d53c0d75a6c397886b993e
unknown <vidhya.1701254@srec.ac.in>
2020-12-31 11:48:05 +0530

    Initial Commit1

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: 'images',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    'gatsby-transformer-remark',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify-cms`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
+import { graphql } from 'gatsby';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
 
-export default IndexPage
+const IndexPage = (props) => {
+  const data = props.data.allFile.edges[0].node.childMarkdownRemark.frontmatter
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>{data.title}</h1>
+      <p>{data.intro}</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <img src={data.image} alt='photo of Ruby' />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
+
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    allFile (filter: {sourceInstanceName: {eq: "content"} name: {eq: "home"}}) {
+      edges {
+        node {
+          childMarkdownRemark {
+            frontmatter {
+              title
+              intro
+              image
+          }
+        }
+      }
+    }
+  }
+}`
